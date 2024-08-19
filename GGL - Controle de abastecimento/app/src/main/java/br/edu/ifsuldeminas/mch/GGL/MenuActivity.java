11cc2c9c74ae025981f7078efcc80396e3be80c3
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2024-08-19 17:49:03 -0300

    First Commit

---
+++ GGL - Controle de abastecimento/app/src/main/java/br/edu/ifsuldeminas/mch/GGL/MenuActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsuldeminas.mch.GGL;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+
+}
